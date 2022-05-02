3985d5f66da754154e766d6db32b33a744fc5100
Bruno Baz <brunobazalvarez@gmail.com>
2022-05-02 15:52:19 +0200

    funciones links db

---
+++ db/links/deleteSinglePost.js
@@ -0,0 +1,15 @@
+const { getConnection } = require('../db');
+
+const deleteSinglePost = async (id) => {
+  let connection;
+  try {
+    connection = await getConnection();
+    await connection.query(`DELETE FROM links WHERE id=?`, [id]);
+    return;
+  } finally {
+    if (connection) {
+      connection.release();
+    }
+  }
+};
+module.exports = { deleteSinglePost };
---
+++ db/links/getSinglePost.js
@@ -0,0 +1,19 @@
+const { getConnection } = require('../db');
+
+const getSinglePost = async (id) => {
+  let connection;
+  try {
+    connection = await getConnection();
+    const [result] = await connection.query(
+      `SELECT links.url, links.titulo, links.descripcion, COUNT(v.post_id) AS votes FROM links, votes v WHERE links.id=v.post_id AND links.id=? GROUP BY enlaces.id`,
+      [id]
+    );
+    return result;
+  } finally {
+    if (connection) {
+      connection.release();
+    }
+  }
+};
+module.exports = { getSinglePost };
+//      `SELECT e.url, e.titulo, e.descripcion, COUNT(v.post_id) FROM enlaces e, votes v WHERE e.id=?`,
