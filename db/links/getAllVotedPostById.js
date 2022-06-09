635c541283c495dff45333e3592dd9b63c997a19
BrunoBaz <brunobazalvarez@gmail.com>
2022-06-09 17:08:11 +0200

    Arreglado imagen usuario

--- db/links/getAllVotedPostById.js
+++ db/links/getAllVotedPostById.js
@@ -6,7 +6,7 @@ const getAllVotedPostById = async (id) => {
     //Añadir con número de likes
     connection = await getConnection();
     const [result] = await connection.query(
-      ` SELECT l.id, l.url, l.titulo, l.descripcion, l.created_at , l.user_id, u.userName, COUNT(v.post_id) AS votes FROM links l LEFT JOIN votes v ON l.id=v.post_id INNER JOIN users u ON l.user_id=u.id  WHERE  l.user_id=? GROUP BY l.id ORDER BY l.id DESC`,
+      ` SELECT l.id, l.url, l.titulo, l.descripcion, l.created_at , l.user_id, u.userName, u.imagen, COUNT(v.post_id) AS votes FROM links l LEFT JOIN votes v ON l.id=v.post_id INNER JOIN users u ON l.user_id=u.id  WHERE  l.user_id=? GROUP BY l.id ORDER BY l.id DESC`,
       [id]
     );
     return result;
